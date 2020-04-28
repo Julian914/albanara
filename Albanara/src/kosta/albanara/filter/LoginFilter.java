cdd7a5a77245a3adece2d9b5836f6abfb61ce2b1
Julian <juliankim914@gmail.com>
2020-04-28 21:44:00 +0900

    로그인 필터 오류 수정

--- Albanara/src/kosta/albanara/filter/LoginFilter.java
+++ Albanara/src/kosta/albanara/filter/LoginFilter.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpSession;
 /**
  * Servlet Filter implementation class LoginFilter
  */
-@WebFilter("/session/*.jsp")
+@WebFilter("/session/*")
 public class LoginFilter implements Filter {
 
 	/**
